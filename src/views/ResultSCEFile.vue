e1233656e1d8c5d8f7e5b37ba785363a8630a140
GeogNum <85230851+G3ogNum@users.noreply.github.com>
2023-04-05 15:46:59 +0800

    4.5
    Signed-off-by: GeogNum <85230851+G3ogNum@users.noreply.github.com>

--- src/views/ResultSCEFile.vue
+++ src/views/ResultSCEFile.vue
@@ -3,31 +3,49 @@
 
     <el-row>
       <el-dialog
-          title="提示"
-          :visible.sync="dialogVisible"
-          width="60%"
-          :before-close="handleClose">
+          title="调整因子"
+          :visible.sync="FactorsDialogVisible"
+          width="80%"
+          :before-close="FactorsHandleClose">
+
         <el-form ref="form" :model="sizeModForm" label-width="300px" size="mini">
-          <el-form-item :label="item[0].factorName" v-for="item in this.ModifyFactor1" style="display: inline-block">
-            <el-select  v-model="sizeModForm.factors[item[0].factorType-1]" placeholder="请选择">
-              <el-option :label="childItem.factorValue+'\t'+childItem.factorDescriptionA"
-               v-for="childItem in item"
-               :value="{factorType:childItem.factorType,
+            <h3 style="margin-left: 10%">功能性特征调整因子</h3>
+            <el-form-item :label="item[0].factorName" v-for="item in this.ModifyFactor1" style="display: inline-block">
+              <el-select value-key="pkId" v-model="sizeModForm.factorList[item[0].factorType-1]" :placeholder="item[0].placeholder?item[0].placeholder:'请选择'">
+                <el-option :label="childItem.factorValue+'\t'+childItem.factorDescriptionA"
+                           v-for="childItem in item"
+                           :key="childItem.pkId"
+                           :value="{factorType:childItem.factorType,
+               factorValue:childItem.factorValue,
+               pkId:childItem.pkId}">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          <h3 style="margin-left: 10%">非功能性特征调整因子</h3>
+          <el-form-item :label="item[0].factorDescriptionA" v-for="item in this.ModifyFactor2" style=" ">
+            <el-select value-key="pkId" v-model="sizeModForm.factorList[item[0].factorType-1]" placeholder="请选择">
+              <el-option :label="childItem.factorValue+'\t'+childItem.factorDescriptionC"
+                         v-for="childItem in item"
+                         :key="childItem.pkId"
+                         :value="{factorType:childItem.factorType,
                factorValue:childItem.factorValue,
                pkId:childItem.pkId}">
               </el-option>
             </el-select>
-          </el-form-item>
-          <el-form-item :label="item[0].factorName+' : '+item[0].factorDescriptionA" v-for="item in this.ModifyFactor2" style="display: inline-block">
-            <el-cascader
-                v-model="sizeModForm.factors[item[0].factorType-1]"
-                :options="options[item[0].factorType-4]">
-            </el-cascader>
             <span class="demonstration"><font color="red"> *</font>{{ ' '+item[0].factorDescriptionB }}</span>
           </el-form-item>
+<!--            <h3 style="margin-left: 10%">非功能性特征调整因子</h3>
+            <el-form-item :label="item[0].factorDescriptionA" v-for="item in this.ModifyFactor2" style="">
+              <el-cascader
+                  v-model="sizeModForm.factorList[item[0].factorType-1]"
+                  :options="options[item[0].factorType-4]">
+              </el-cascader>
+              <span class="demonstration"><font color="red"> *</font>{{ ' '+item[0].factorDescriptionB }}</span>
+            </el-form-item>-->
+
           <el-form-item size="large" style="margin-left:25%;margin-top: 3%">
-            <el-button type="primary" @click="onSubmit">提交</el-button>
-            <el-button  @click="dialogVisible=false">取消</el-button>
+            <el-button type="primary" @click="onFactorSubmit">提交</el-button>
+            <el-button  @click="FactorsDialogVisible=false">取消</el-button>
           </el-form-item>
         </el-form>
       </el-dialog>
@@ -40,7 +58,7 @@
             <el-button type="primary" @click="dialogVisible=true">
               成本评估
             </el-button>
-            <el-button type="primary" @click="dialogVisible=true">
+            <el-button type="primary" @click="FactorsDialogVisible=true">
               调整因子
             </el-button>
             <el-button type="primary" @click="dialogVisible=true">
@@ -125,10 +143,12 @@
 <script>
 import http from "@/utils/request";
 import myFilter from "@/utils/filter";
+import Cookie from "js-cookie";
+import {string} from "mockjs/src/mock/random/basic";
 let id = 1000;
 
 export default {
-  data() {
+  data: function () {
     const generateData = _ => {
       const data = [];
       for (let i = 1; i <= 15; i++) {
@@ -141,96 +161,127 @@ export default {
       return data;
     };
     return {
-      ModifyFactor:[],
-      ModifyFactor1:[],
-      ModifyFactor2:[],
-      options:[
-          [
-            {
-              label:'-1 没有明示对分布式处理的需求事项',
-              value:{factorType:4,
-                factorValue:-1,
-                pkId:16}
-            },
-            {
-              label:' 0 通过网络进行客户端/服务器及网络基础计算机系统分布处理和传输',
-              value:{factorType:4,
-                factorValue:0,
-                pkId:17}
-            },
-            {
-              label:' 1 在多个服务器及处理器上同时相互执行计算机系统中的处理功能',
-              value:{factorType:4,
-                factorValue:1,
-                pkId:18}
-            },
-          ],
+
+      ModifyFactor: [],
+      ModifyFactor1: [],
+      ModifyFactor2: [],
+      chosenFactor:[],
+      options: [
         [
           {
-            label:'-1 没有明示对性能的特别需求事项或活动，因此提供基本性能',
-            value:{factorType:4,
-              factorValue:-1,
-              pkId:19}
+            label: '-1 没有明示对分布式处理的需求事项',
+            value: {
+              factorType: 4,
+              factorValue: -1,
+              pkId: 16
+            }
           },
           {
-            label:' 0 应答时间或处理率对高峰时间或所有业务时间都很重要，对连动系统结 束处理时间有限制',
-            value:{factorType:4,
-              factorValue:0,
-              pkId:20}
+            label: ' 0 通过网络进行客户端/服务器及网络基础计算机系统分布处理和传输',
+            value: {
+              factorType: 4,
+              factorValue: 0,
+              pkId: 17
+            }
           },
           {
-            label:' 1 为满足性能需求事项，要求设计阶段进行性能分析，或在设计、开发阶 段使用分析工具',
-            value:{factorType:4,
-              factorValue:1,
-              pkId:21}
+            label: ' 1 在多个服务器及处理器上同时相互执行计算机系统中的处理功能',
+            value: {
+              factorType: 4,
+              factorValue: 1,
+              pkId: 18
+            }
           },
         ],
         [
           {
-            label:'-1 没有明示对可靠性的特别需求事项或活动，因此提供基本的可靠性',
-            value:{factorType:4,
-              factorValue:-1,
-              pkId:22}
+            label: '-1 没有明示对性能的特别需求事项或活动，因此提供基本性能',
+            value: {
+              factorType: 4,
+              factorValue: -1,
+              pkId: 19
+            }
           },
           {
-            label:' 0 发生故障时可轻易修复，带来一定不便或经济损失',
-            value:{factorType:4,
-              factorValue:0,
-              pkId:23}
+            label: ' 0 应答时间或处理率对高峰时间或所有业务时间都很重要，对连动系统结 束处理时间有限制',
+            value: {
+              factorType: 4,
+              factorValue: 0,
+              pkId: 20
+            }
           },
           {
-            label:' 1 发生故障时很难修复，发生重大经济损失或有生命危害',
-            value:{factorType:4,
-              factorValue:1,
-              pkId:24}
+            label: ' 1 为满足性能需求事项，要求设计阶段进行性能分析，或在设计、开发阶 段使用分析工具',
+            value: {
+              factorType: 4,
+              factorValue: 1,
+              pkId: 21
+            }
           },
         ],
         [
           {
-            label:'-1 在相同用途的硬件或软件环境下运行',
-            value:{factorType:4,
-              factorValue:-1,
-              pkId:25}
+            label: '-1 没有明示对可靠性的特别需求事项或活动，因此提供基本的可靠性',
+            value: {
+              factorType: 4,
+              factorValue: -1,
+              pkId: 22
+            }
           },
           {
-            label:' 0 在用途类似的硬件或软件环境下运行',
-            value:{factorType:4,
-              factorValue:0,
-              pkId:26}
+            label: ' 0 发生故障时可轻易修复，带来一定不便或经济损失',
+            value: {
+              factorType: 4,
+              factorValue: 0,
+              pkId: 23
+            }
           },
           {
-            label:' 1 在不同用途的硬件或软件环境下运行',
-            value:{factorType:4,
-              factorValue:1,
-              pkId:27}
+            label: ' 1 发生故障时很难修复，发生重大经济损失或有生命危害',
+            value: {
+              factorType: 4,
+              factorValue: 1,
+              pkId: 24
+            }
+          },
+        ],
+        [
+          {
+            label: '-1 在相同用途的硬件或软件环境下运行',
+            value: {
+              factorType: 4,
+              factorValue: -1,
+              pkId: 25
+            }
+          },
+          {
+            label: ' 0 在用途类似的硬件或软件环境下运行',
+            value: {
+              factorType: 4,
+              factorValue: 0,
+              pkId: 26
+            }
+          },
+          {
+            label: ' 1 在不同用途的硬件或软件环境下运行',
+            value: {
+              factorType: 4,
+              factorValue: 1,
+              pkId: 27
+            }
           },
         ],
       ],
-      sizeModForm:{
-          factors:[
-          ],
-        },
+      sizeModForm: {
+        factorList: [],
+        projectId: Cookie.get('projectId'),
+      },
+      finalForm: {
+        factorList: [],
+        projectId: Cookie.get('projectId'),
+      },
       dialogVisible: false,
+      FactorsDialogVisible: false,
       data: generateData(),
       value: [1, 4],
       showData: [],
@@ -310,14 +361,33 @@ export default {
   },
 
   methods: {
+    string,
 
-    onSubmit() {
-      console.log(this.sizeModForm.factors)
-      this.handleClose()
+    onFactorSubmit :function() {
+      let cnt=0;
+      for(let i=0; i<this.sizeModForm.factorList.length; i++){
+        if(this.sizeModForm.factorList[i]!=null){
+          this.finalForm.factorList[cnt++]=this.sizeModForm.factorList[i]
+        }
+      }
+      /*this.sizeModForm.factorList.forEach(function (item) {
+        if(item!=null){
+          this.finalForm.factorList.add(item);
+        }
+      })*/
+      console.log(this.finalForm)
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          http.post('http://192.168.159.240:25005/pluto/addProjectFactor',this.finalForm)
 
+
+        }
+      })
+      this.FactorsHandleClose()
     },
-    handleClose(done) {
-      this.dialogVisible=false
+
+    FactorsHandleClose(done) {
+      this.FactorsDialogVisible=false
 
     },
     treeClick(node) {
@@ -334,20 +404,18 @@ export default {
       data.children.push(newChild);
     },
     getList() {
-      http.post('http://192.168.159.240:25005/neptune/queryAllFactors').then(({data}) => {
+      http.post('http://192.168.159.240:25005/pluto/queryAllFactors').then(({data}) => {
         this.ModifyFactor = data.data
-       /* console.log(this.ModifyFactor[0])*/
-        /*this.ModifyFactor.forEach((Data)=>{
-          Data.forEach((facInfo)=>{
-            facInfo.factorValue=myFilter.filters.number(facInfo.factorValue)
-            if(facInfo.factorDescriptionB){
-              facInfo.factorDescriptionA=facInfo.factorDescriptionA+'，'+facInfo.factorDescriptionB
-            }
-            if(facInfo.factorDescriptionC){
-              facInfo.factorDescriptionA=facInfo.factorDescriptionA+'，'+facInfo.factorDescriptionC
-            }
-          })
-        })*/
+        http.post('http://192.168.159.240:25005/pluto/queryProjectFactorList', {projectId: Cookie.get('projectId')}).then(({data}) => {
+          this.chosenFactor=data.data
+          for(let i=0;i<this.chosenFactor.length;i++){
+            if(this.chosenFactor[i].factorType>3&&this.chosenFactor[i].factorType<9)
+            this.$set(this.ModifyFactor[this.chosenFactor[i].factorType-1][0],"placeholder",this.chosenFactor[i].factorValue+' '+this.chosenFactor[i].factorDescriptionC);
+            else
+              this.$set(this.ModifyFactor[this.chosenFactor[i].factorType-1][0],"placeholder",this.chosenFactor[i].factorValue+' '+this.chosenFactor[i].factorDescriptionA);
+          }
+
+        })
         let cnt1=0;
         let cnt2=0;
         for(let i=0;i<this.ModifyFactor.length;i++){
@@ -357,6 +425,7 @@ export default {
             this.ModifyFactor1[cnt1++]=this.ModifyFactor[i]
           }
         }
+        console.log(this.ModifyFactor1)
         console.log(this.ModifyFactor2)
       })
       http.post('http://192.168.159.240:25005/pluto/nep/queryPlutoNEPList', {projectId: 1/*Cookie.get('projectId')*/}).then(({data}) => {
